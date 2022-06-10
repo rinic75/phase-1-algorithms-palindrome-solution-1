480d007c73e13a30717ade02458c45209fe8aa90
Chang Shin <cshin7374@gmail.com>
2022-06-09 23:21:14 -0500

    commit tree 082680605b73f1c9db07ec2e1a6981fb2bf9c232

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const reversedWord = reversed(word);
+  return word === reversedWord;
+
+}
+function reversed(word) {
+  return word.split("").reverse().join("");
 }
 
 /* 
@@ -16,7 +22,8 @@ if (require.main === module) {
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mommom"))
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
